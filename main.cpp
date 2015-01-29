7f00f33e44c6ea7b4031da92d85524001ecdf3fd
jnowicki <kubavic@interia.pl>
2015-01-29 22:52:36 +0100

    configurable holes

--- main.cpp
+++ main.cpp
@@ -163,38 +163,22 @@ using namespace std;
 
 int main(){	
 	// generowanie dziur	
-    cout << "Generator: generating holes" << endl;
-    cout << "Generator: position1, position2" << endl;
+    int hole_t;
+    int hole_l;
+    
+    cout << "Give me length of hole: (recommended is 2)" << std::endl;
+    cin >> hole_l;
+    
+    cout << "Give me interval between holes: (recommended is 4)" << std::endl;
+    cin >> hole_t;
+    
+	// generowanie dziur	
+    cout << "Generator: generating holes for hole_l = " << hole_l << ", hole_t = " << hole_t << endl;
 
-    vector<int> holePos = generateHoles(HOLE_T, HOLE_L); // deklaracja i inicjacja tablicy pozycji dziur
+    vector<int> holePos = generateHoles(hole_t, hole_l); // deklaracja i inicjacja tablicy pozycji dziur
 
     srand(time(NULL));
-    /*
-    std::vector<int> jobs = generateJobs(JOBS_QUANTITY);
-    std::vector<jobPair> pairArr;
-    for (int i = 0; i < pairArr.size(); i++) {
-        for (int j = 0; j < jobs.size(); j++) {
-            if (i != j) {
-                std::pair <int, int> lengths(jobs.at(i), jobs.at(j));
-                std::pair <int, int> idents(i + 1, j + 1);
-                jobPair newPair(lengths, idents);
-                pairArr.push_back(newPair);
-            }
-        };
-    };
-
-    Machine first = Machine(1);
-    Machine second = Machine(2, holePos);
-    Overseer test(first, second, 1, pairArr);
-
-
-
-    for (int i = 0; i < 3; i++) {
-        test.first.addJob(jobs.at(i%2), i%2);
-        test.second.addJob(jobs.at(i%2+1), i%2+1);
-        test.doJobs(i);
-    };
-     */
+    
     algorithm(generateJobs(JOBS_QUANTITY), holePos);
 
     return 0;
